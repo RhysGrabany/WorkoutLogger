4ec2eaa9bcb0b4054ec3e05faddb3fae829de5f5
Rhys Grabany <r.grabany@gmail.com>
2020-08-10 18:30:12 +0100

    Add project files.

---
+++ WorkoutLoggerLibrary/ExerciseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkoutLoggerLibrary
+{
+	class ExerciseModel
+	{
+		public string ExerciseName { get; set; }
+	}
+}
