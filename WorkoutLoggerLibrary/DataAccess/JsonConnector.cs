9408fe9bb9afe293393128e0e2677c7e7f99891b
Rhys Grabany <r.grabany@gmail.com>
2020-08-24 19:38:49 +0100

    Adding JsonConnector and it's helper class

---
+++ WorkoutLoggerLibrary/DataAccess/JsonConnector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkoutLoggerLibrary.Models;
+
+namespace WorkoutLoggerLibrary.DataAccess
+{
+    class JsonConnector : IDataConnection
+    {
+        // TODO - Wire up CreateDay for Binary files
+        /// <summary>
+        /// Saves a day to a binary file
+        /// </summary>
+        /// <param name="model">The day info</param>
+        /// <returns>The day model with unique id</returns>
+        public DateModel CreateDay(DateModel model)
+        {
+            return model;
+        }
+
+        public TemplateModel CreateTemplate(TemplateModel model)
+        {
+            return model;
+        }
+
+        public DateModel LoadDate(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TemplateModel LoadTemplate(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
